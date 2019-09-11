87bdd548ba963da225c9b0313bd711b03e1579a9
ydlpizarro <yohel.larrpizarro@gmail.com>
2019-09-11 19:40:05 -0300

    first commit

---
+++ src/cadastro/Usuario.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastro;
+
+/**
+ *
+ * @author DS4i
+ */
+public class Usuario {
+	private String nome;
+	private String login;
+	private String senha;
+	private int nivel;//1-Administrador e 2-Comum
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+	
+	
+}
